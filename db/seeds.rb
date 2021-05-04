6810a3089871db09edb77746f10098712d0e2fdf
RL2020 <loc.fitness1@gmail.com>
2021-05-04 09:56:07 -0400

    commit tree 61c8cdc6dec735f6a2f34afa6b3d3304aa8d1691

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  
+a = Article.create(title: "Borat", description: "Great read")
+
+puts "You got a seed"
